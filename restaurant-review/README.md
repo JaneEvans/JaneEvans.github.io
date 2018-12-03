5e13375a36d3a1cbb2678aafb3bff615cb5770ca
Jinjin <jige@expedia.com>
2018-12-02 21:13:05 -0800

    Add new project: restaurant review

---
+++ restaurant-review/README.md
@@ -0,0 +1,18 @@
+# Restaurant Review Project
+A restaurant review website that can be --
+1. responsive on different sized displays
+2. accessible for screen reader using `Tab`
+3. and able to process seamless offline experience for users.
+
+## How to To Run This Website locally
+1. You need to start up a simple HTTP server to serve up the site files on your local computer. Python has some simple tools to do this.
+2. In a terminal, check the version of Python you have: `python -V`. If you have Python 2.x, spin up the server with `python -m SimpleHTTPServer 8000` (or some other port, if port 8000 is already in use.) For Python 3.x, you can use `python3 -m http.server 8000`. If you don't have Python installed, navigate to Python's website to download and install the software.
+3. For Windows systems, Python 3.x is installed as python by default. To start a Python 3.x server, you can simply enter `python -m http.server 8000`.
+4. With your server running, visit the site: http://localhost:8000. 
+
+## Author
+* **Jinjin Ge** - [JaneEvans](https://janeevans.github.io/my-portfolio-website)
+
+## Acknowledgments
+* [MATTHEW CRANFORD](https://matthewcranford.com/)
+* [CSS Tricks](https://css-tricks.com/almanac/)
---
+++ restaurant-review/sw.js
@@ -0,0 +1,47 @@
+//Indicate the service worker is registered.
+console.log('Service Worker is registered.');
+
+//Install cache and add cache files into the cache storage
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open('vw1').then(function(cache){
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/data/restaurants.json',
+                '/css/styles.css',
+                '/js/dbhelper.js',
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg'
+            ]);
+        })
+    );
+});
+
+//Fetching the events
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        // Check if the event request url already exists in the cache
+        caches.match(e.request).then(function(response){
+            if (response) {
+                console.log('Found', e.request, ' in cache');
+                return response;
+            }
+            else {
+                console.log('Cannot find', e.request, 'in cache, Fetching...');
+                return fetch(e.request);
+            }
+        })
+    );
+});

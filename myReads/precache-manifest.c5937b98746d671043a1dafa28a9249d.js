66df27b04b51d2b43178eeb89172a69ae6e88e49
Jinjin <jige@expedia.com>
2018-12-15 17:04:45 -0800

    add myReads

---
+++ myReads/precache-manifest.c5937b98746d671043a1dafa28a9249d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "27d1eb6964042f3c50bc",
+    "url": "/myReads/static/js/runtime~main.27d1eb69.js"
+  },
+  {
+    "revision": "ac9f44ec6797bb9b6ee0",
+    "url": "/myReads/static/js/main.ac9f44ec.chunk.js"
+  },
+  {
+    "revision": "1cf998e46cbda6b803e1",
+    "url": "/myReads/static/js/1.1cf998e4.chunk.js"
+  },
+  {
+    "revision": "ac9f44ec6797bb9b6ee0",
+    "url": "/myReads/static/css/main.e24dcb1d.chunk.css"
+  },
+  {
+    "revision": "a72e3ac57efb12ebc472b560e307fed8",
+    "url": "/myReads/index.html"
+  }
+];

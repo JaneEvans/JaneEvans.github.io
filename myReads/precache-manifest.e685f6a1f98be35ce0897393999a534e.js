7aa0b86fa16923150e17181ab6cb741c62b21558
Jinjin <jige@expedia.com>
2018-12-15 17:39:40 -0800

    add myReads

---
+++ myReads/precache-manifest.e685f6a1f98be35ce0897393999a534e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "./static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "./static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "./static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4ec0e17874fa5144ecdb",
+    "url": "./static/js/main.4ec0e178.chunk.js"
+  },
+  {
+    "revision": "8373b944866c613c9f5d",
+    "url": "./static/js/1.8373b944.chunk.js"
+  },
+  {
+    "revision": "4ec0e17874fa5144ecdb",
+    "url": "./static/css/main.bfb97725.chunk.css"
+  },
+  {
+    "revision": "94a5217c4ef0cf78c6e37d308667c119",
+    "url": "./index.html"
+  }
+];

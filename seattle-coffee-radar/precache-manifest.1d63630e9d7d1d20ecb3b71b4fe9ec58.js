0f62f16fd35fa265812295eaa4511bbceee46833
Jinjin <jige@expedia.com>
2019-01-08 22:50:25 -0800

    add seattle coffee radar project

---
+++ seattle-coffee-radar/precache-manifest.1d63630e9d7d1d20ecb3b71b4fe9ec58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d08e383239b1b730ebeeb5e6edb54329",
+    "url": "./static/media/starbucks_icon.d08e3832.png"
+  },
+  {
+    "revision": "352f327abde78a63ad7fde20398a76fd",
+    "url": "./static/media/coffee_icon.352f327a.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3cd28d3b365c8a102c05",
+    "url": "./static/js/main.3cd28d3b.chunk.js"
+  },
+  {
+    "revision": "e1437e8a6e1b054d9c6d",
+    "url": "./static/js/1.e1437e8a.chunk.js"
+  },
+  {
+    "revision": "3cd28d3b365c8a102c05",
+    "url": "./static/css/main.6bcaff45.chunk.css"
+  },
+  {
+    "revision": "bfad6dc6c1133658e5b1b05267af9764",
+    "url": "./index.html"
+  }
+];
